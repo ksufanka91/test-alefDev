bf8ce560c590a35db1f7f3689a7aff65ff594674
kseniia <ksufanka91@mail.ru>
2022-02-06 08:48:41 +0300

    adaptive

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var main = new Splide('#main-slider', {
-    type: 'fade',
-    rewind: true,
-    pagination: false,
-    arrows: false,
-    width: '100%',
-    heightRatio: 1.28,
-  });
+    var main = new Splide('#main-slider', {
+        type: 'fade',
+        rewind: true,
+        pagination: false,
+        arrows: false,
+        width: '100%',
+        heightRatio: 1.28,
+    });
 
-  var thumbnails = new Splide('#thumbnail-slider', {
-    fixedWidth: 70,
-    fixedHeight: 91,
-    gap: 7,
-    rewind: true,
-    pagination: false,
-    cover: true,
-    direction: 'ttb',
-    height: 483,
-    arrows: false,
-    isNavigation: true,
-    breakpoints: {
-      600: {
-        fixedWidth: 60,
-        fixedHeight: 44,
-      },
-    },
-  });
+    var thumbnails = new Splide('#thumbnail-slider', {
+        fixedWidth: 70,
+        fixedHeight: 91, // 1.3 соотношение
+        gap: 7,
+        rewind: true,
+        pagination: false,
+        cover: true,
+        direction: 'ttb',
+        height: 483,
+        arrows: false,
+        isNavigation: true,
+        breakpoints: {
+            900: {
+                fixedWidth: 60,
+                fixedHeight: 78,
+            },
+            425: {
+                fixedWidth: 40,
+                fixedHeight: 52,
+            },
+        },
+    });
 
-  main.sync(thumbnails);
-  main.mount();
-  thumbnails.mount();
+    main.sync(thumbnails);
+    main.mount();
+    thumbnails.mount();
 });
